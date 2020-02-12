3225c214326535673c50be28c857cc3b45ec4051
Jared Madris <jaredmadris@gmail.com>
2020-02-11 21:24:36 -0500

    fixed

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,84 +1,44 @@
-// From data.js
+// from data.js
 var tableData = data;
 
-// Select tbody 
-tbody = d3.select("tbody")
-console.log("hello")
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
 
-// Loop through table
-function displayData(something){ 
-    tbody.text("")
-    something.forEach(function(et_sighting){
-    new_tr = tbody.append("tr")
-    Object.entries(et_sighting).forEach(function([key, value]){
-        new_td = new_tr.append("td").text(value)	
-    })
-})}
+// Function build table
+function buildTable(data){
+    // First, clear out existing data
+    tbody.html("");
+    data.forEach(dataRow => {
+        console.table(dataRow);
+        let row = tbody.append("tr");
 
-displayData(tableData)
-
-console.log("hello2")
-
-
-// Submit button
-var submit = d3.select("#submit");
-
-submit.on("click", function() {
-    console.log("hello3")
-
-  // Prevent page from refreshing
-  d3.event.preventDefault();
-
-  // Select the input element and get the raw HTML node
-  var dateInput = d3.select("#datetime");
-  var cityInput = d3.select("#city");
-  var stateInput = d3.select("#state");
-  var countryInput = d3.select("#country");
-  var shapeInput = d3.select("#shape");
-
-  // Value property of the input element
-  console.log(dateInput.property("value"));
-  console.log(cityInput.property("value"));
-  console.log(stateInput.property("value"));
-  console.log(countryInput.property("value"));
-  console.log(shapeInput.property("value"));
-
-  //Create variable filters
-
- var filtered = tableData.filter(et_sighting =>{
-  return (et_sighting.datetime===dateInput.property("value") || !dateInput.property("value") ) && 
-            (et_sighting.city===cityInput.property("value") || !cityInput.property("value")) &&
-            (et_sighting.state===stateInput.property("value") || !stateInput.property("value")) &&
-            (et_sighting.country===countryInput.property("value") || !countryInput.property("value")) &&
-            (et_sighting.shape===shapeInput.property("value") || !shapeInput.property("value"))
- })
-
- //Runfiltered entries through displayData function to update the table
- displayData(filtered);
-
-
-});
-
-var filterInputs = d3.selectAll('.form-control');
-
-// Clears input fields and input object
-function clearEntries() {
-    filters = {};
-
-    // Sets every input field to empty
-    filterInputs._groups[0].forEach(entry => {
-        if (entry.value != 0) {
-            d3.select('#' + entry.id).node().value = "";
-        }
+       console.table(Object.values(dataRow));
+       Object.values(dataRow).forEach((val) => {
+           let cell = row.append("td");
+           cell.text(val);
+       });
     });
-};
+}
 
-var clearButton = d3.select("#clear");
-// Clear button on click clears fields
-clearButton.on('click', function () {
+// Event that calls a function  name it handleClick
+function handleClick(){
+    d3.event.preventDefault() // prevent the form from refreshing the page
+    
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
 
-    // Keeps page from refreshing completely, only want the table to refresh
-    d3.event.preventDefault();
-    // Clears input fields
-    clearEntries()
-});
+    // Check to see if a date was entered and filter the data using that date
+    if (date){
+        // Apply filter to the table data to only keep the 
+        // rows where the datetime value matches the filter value
+        filterData = filterData.filter((row) => row.datetime === date);
+
+    }
+
+    buildTable(filterData);
+}
+
+
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData);
